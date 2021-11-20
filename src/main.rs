7bfa77a7a1e2055c0d5c3ff6c35d712634982b71
Tiago-0liveira <tfgoliveira@gmail.com>
2021-11-20 22:36:58 +0000

    command detection finnished

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,39 @@
 #![allow(non_snake_case)]
 mod Command;
 
-use Command::command::StaticCommand;
-
 use crate::Command::command;
 use std;
-use std::io::{Write, stdin, stdout};
-use std::env;
+use std::env::{self, Args};
 use std::process::exit;
 
 fn main() {
     if env::args().len() > 1 {
-        for argument in env::args().skip(1) {
-            process_cli_arg(argument)        
-        }
+        process_cli_args(env::args());
     } else {
-        let mut input = String::new();
-        let quit = String::from("quit");
-        while !&input.trim().to_lowercase().ends_with(&quit) {
-            print!("> ");
-            let _ = stdout().flush();
-            match stdin().read_line(&mut input) {
-                Ok(_) => (),
-                Err(error) => {
-                    println!("Error: {}", error);
-                    exit(1)
-                },
-            }
-        }
+        show_help();
     }
 }
 
 fn show_help() {
-    command::get_static_commands().iter().for_each(|c| {
+    let coms = &command::get_commands();
+    coms.iter().for_each(|c| {
         println!("{}", c);
     });
 }
 
-fn process_cli_arg(arg: String) {
-    let command = command::get_static_commands().iter().find(|c| c.name == arg);
-    if let Some(command) = command {
-        println!("arg: {}", arg);
-        if arg.starts_with("-n:") || arg.starts_with("--new:") {
-            let new_line = arg.split(":").nth(1).unwrap();
-            println!("parsed arg -> {}", new_line);
-        } else if arg.starts_with("-h") || arg.starts_with("--help") {
-            show_help();
-            exit(0);   
-        } else if arg.starts_with("-l") || arg.starts_with("--list") {
-            println!("-l --list");
-            exit(0);
-        }
-    } else {
-        println!("Command not found: {}", arg);
+fn process_cli_args(args: Args) {
+    let coms = &command::get_commands();
+    let mut first_arg = args.skip(1).next().unwrap();
+    first_arg = (&first_arg.trim_start_matches("-")).to_string();
+    let com = coms.iter().find(|c|  
+        c.name == (&first_arg).to_string() ||
+        c.special_args.1 == (&first_arg).to_string() ||
+        c.special_args.0.to_string() == (&first_arg).to_string()
+    );
+    if com.is_none() {  
+        println!("Command {} not found", first_arg);
+        exit(1);
     }
+    let com = com.unwrap();
+    println!("{}", com);
 }
