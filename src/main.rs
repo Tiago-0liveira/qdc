315832aef4e0f004463500889570b9cb872c7244
Tiago-0liveira <tfgoliveira@gmail.com>
2021-11-21 03:32:13 +0000

    start implementation of comands and json storing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 #![allow(non_snake_case)]
 mod Command;
+mod Helper;
 
 use crate::Command::command;
+use crate::Helper::helpers;
 use std;
-use std::env::{self, Args};
+use std::env;
+use std::path::Path;
 use std::process::exit;
+use colored::*;
 
 fn main() {
-    if env::args().len() > 1 {
-        process_cli_args(env::args());
+    let args = env::args().collect::<Vec<String>>();
+    if (&args).len() > 1 {
+        process_cli_args(args);
     } else {
         show_help();
     }
@@ -21,19 +26,66 @@ fn show_help() {
     });
 }
 
-fn process_cli_args(args: Args) {
+fn process_cli_args(args: Vec<String>) {
     let coms = &command::get_commands();
-    let mut first_arg = args.skip(1).next().unwrap();
-    first_arg = (&first_arg.trim_start_matches("-")).to_string();
-    let com = coms.iter().find(|c|  
+    let first_arg = (&args)[1].trim_start_matches("-").trim().to_string();
+    let com = coms.iter().find(|c|
         c.name == (&first_arg).to_string() ||
         c.special_args.1 == (&first_arg).to_string() ||
         c.special_args.0.to_string() == (&first_arg).to_string()
     );
-    if com.is_none() {  
-        println!("Command {} not found", first_arg);
+    if com.is_none() { 
+        println!("Command {} not found", first_arg.red());
         exit(1);
     }
     let com = com.unwrap();
-    println!("{}", com);
+    if (&com).args_num > (&args).len() - 2 {
+        println!("Command {} requires {} arguments, args num: {}, provided args: '{:?}'", (&com).name.cyan(), (&com).args_num.to_string().green(), (args.len() - 2).to_string().red(), args[2..].to_vec());
+        exit(1);
+    }
+    let args = (&args)[2..].to_vec();
+    let mut file_contents = helpers::get_file_contents();
+    let shortcutNames = helpers::get_shortcut_names(&file_contents);
+    let shortcutPaths = helpers::get_shortcut_paths(&file_contents);
+    match com.name.as_str() {
+        "new" => {
+            if !args[0].trim().is_empty() {
+                let shortcutName = args[0].trim();
+                if !shortcutNames.contains(&shortcutName.to_string()) {
+                    let path = args[1].trim();
+                    if !shortcutPaths.contains(&path.to_string()) {
+                        if Path::new(&path).exists() {
+                            if Path::new(&path).is_dir() {
+                                file_contents.push(helpers::Record::new((&shortcutName).to_string(), (&path).to_string()));
+                                println!("Shortcut {} created", shortcutName.green());
+                            } else {
+                                println!("Path {} is not a directory", path.red());
+                                exit(1);
+                            }
+                        } else {
+                            println!("Path {} is invalid!", path.red());
+                            exit(1);
+                        }
+                    } else {
+                        println!("Shortcut {} already exists", shortcutName.red());
+                        exit(1);
+                    }
+                } else {
+                    println!("Shortcut {} already exists", shortcutName.red());
+                    exit(1);
+                }
+            } else {
+                println!("Shortcut name is required");
+                exit(1);
+            }
+        },
+        "help" => {},
+        "shortcuts" => {},
+        "edit" => {},
+        "delete" => {},
+        _ => {
+            println!("Command {} not found!", first_arg.red());
+            exit(1);
+        }
+    };
 }
